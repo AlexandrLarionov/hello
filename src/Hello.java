2656e803be26da075213337d33c4f711b9149aff
Александр <sashalarionov2222@mail.ru>
2021-06-09 20:52:42 +0500

    Add 2 AAA

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("HEllo world");
         System.out.println("HEllo world 2");
+        System.out.println("HEllo world");
     }
 }
